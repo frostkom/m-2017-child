5130107a52a57c689334d582203b7019de947fbc
frostkom <martin.fors@frostkom.se>
2017-08-16 11:21:13 +0200

    First commit

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php 
+
+add_action('wp_head', 'head_child', 0);
+
+function head_child()
+{
+	mf_enqueue_style('child-style', get_stylesheet_directory_uri()."/style.css", get_plugin_version(__FILE__)); //, array('style')
+}
