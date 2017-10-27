ded786a36cf1d825d83f7b62cb24ef071a0c20fd
frostkom <martin.fors@frostkom.se>
2017-10-27 17:02:36 +0200

    Use theme version when enqueing style

--- functions.php
+++ functions.php
@@ -4,5 +4,5 @@ add_action('wp_head', 'head_child', 0);
 
 function head_child()
 {
-	mf_enqueue_style('child-style', get_stylesheet_directory_uri()."/style.css", get_plugin_version(__FILE__)); //, array('style')
+	mf_enqueue_style('child-style', get_stylesheet_directory_uri()."/style.css", get_theme_version()); //, array('style')
 }
