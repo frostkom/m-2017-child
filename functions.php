dec7082b3aac424699e8c9323d56ca6046e2abdb
Martin Fors <martin.fors@frostkom.se>
2018-04-06 15:05:40 +0200

    Remove style dependency

--- functions.php
+++ functions.php
@@ -4,5 +4,5 @@ add_action('wp_head', 'head_child', 0);
 
 function head_child()
 {
-	mf_enqueue_style('child-style', get_stylesheet_directory_uri()."/style.css", get_theme_version()); //, array('style')
+	mf_enqueue_style('child-style', get_stylesheet_directory_uri()."/style.css", get_theme_version());
 }
