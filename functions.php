3402b7c78f8b5573d67f1f71a72943dbdc57476e
frostkom <martin.fors@frostkom.se>
2020-10-22 08:21:48 +0200

    Add function to disable a specific plugin that will crash sites

--- functions.php
+++ functions.php
@@ -1,21 +1,15 @@
 <?php
 
-add_action('wp_head', 'head_child', 0);
+include_once("include/classes.php");
 
-function head_child()
+$obj_theme_child = new mf_theme_child();
+
+if(is_admin())
 {
-	$theme_include_url = get_stylesheet_directory_uri()."/";
-	$theme_version = get_theme_version();
+	add_filter('site_transient_update_plugins', array($obj_theme_child, 'site_transient_update_plugins'));
+}
 
-	mf_enqueue_style('child-style', $theme_include_url."style.css", $theme_version);
-
-	echo "<link rel='apple-touch-icon' sizes='180x180' href='".$theme_include_url."favicon/apple-touch-icon.png'>
-	<link rel='icon' type='image/png' sizes='32x32' href='".$theme_include_url."favicon/favicon-32x32.png'>
-	<link rel='icon' type='image/png' sizes='16x16' href='".$theme_include_url."favicon/favicon-16x16.png'>
-	<link rel='manifest' href='".$theme_include_url."favicon/site.webmanifest'>
-	<link rel='mask-icon' href='".$theme_include_url."favicon/safari-pinned-tab.svg' color='#5bbad5'>
-	<link rel='shortcut icon' href='".$theme_include_url."favicon/favicon.ico'>
-	<meta name='msapplication-TileColor' content='#ffffff'>
-	<meta name='msapplication-config' content='".$theme_include_url."favicon/browserconfig.xml'>
-	<meta name='theme-color' content='#ffffff'>";
+else
+{
+	add_action('wp_head', array($obj_theme_child, 'wp_head'), 0);
 }
---
+++ include/classes.php
@@ -0,0 +1,39 @@
+<?php
+
+class mf_theme_child
+{
+	function __construct(){}
+
+	function site_transient_update_plugins($value)
+	{
+		$arr_plugins = array('wp-live-chat-support/wp-live-chat-support.php');
+
+		foreach($arr_plugins as $plugin)
+		{
+			if(isset($value->response[$plugin]))
+			{
+				unset($value->response[$plugin]);
+			}
+		}
+
+		return $value;
+	}
+
+	function wp_head()
+	{
+		$theme_include_url = get_stylesheet_directory_uri()."/";
+		$theme_version = get_theme_version();
+
+		mf_enqueue_style('child-style', $theme_include_url."style.css", $theme_version);
+
+		echo "<link rel='apple-touch-icon' sizes='180x180' href='".$theme_include_url."favicon/apple-touch-icon.png'>
+		<link rel='icon' type='image/png' sizes='32x32' href='".$theme_include_url."favicon/favicon-32x32.png'>
+		<link rel='icon' type='image/png' sizes='16x16' href='".$theme_include_url."favicon/favicon-16x16.png'>
+		<link rel='manifest' href='".$theme_include_url."favicon/site.webmanifest'>
+		<link rel='mask-icon' href='".$theme_include_url."favicon/safari-pinned-tab.svg' color='#5bbad5'>
+		<link rel='shortcut icon' href='".$theme_include_url."favicon/favicon.ico'>
+		<meta name='msapplication-TileColor' content='#ffffff'>
+		<meta name='msapplication-config' content='".$theme_include_url."favicon/browserconfig.xml'>
+		<meta name='theme-color' content='#ffffff'>";
+	}
+}
