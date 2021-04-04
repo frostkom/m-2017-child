9add4bd764d2463cef2c6924d58e594baf55f526
Martin Fors <martin.fors@frostkom.se>
2021-04-04 16:49:30 +0200

    Add dashboard widget if Postie is active

--- include/classes.php
+++ include/classes.php
@@ -2,7 +2,10 @@
 
 class mf_theme_child
 {
-	function __construct(){}
+	function __construct()
+	{
+		$this->lang_key = 'lang_m_child';
+	}
 
 	function site_transient_update_plugins($value)
 	{
@@ -27,8 +30,40 @@ class mf_theme_child
 		if($setting_base_template_site != '')
 		{
 			$menu_link = str_replace("mall.", "", $setting_base_template_site)."/manual/";
-			$menu_title = __("Manual", 'lang_m_2017_child');
+			$menu_title = __("Manual", $this->lang_key);
 			add_menu_page("", $menu_title, 'read', $menu_link, '', 'dashicons-sos', 100);
+		}
+	}
+
+	function add_widget()
+	{
+		if(is_plugin_active("postie/postie.php"))
+		{
+			$widget_title = __("Create posts by sending e-mail message", $this->lang_key);
+
+			//add_meta_box("custom_dashboard_widget_postie", $widget_title, array($this, 'display_widget'), 'dashboard', 'normal', 'default'); //, $post_id
+			wp_add_dashboard_widget("custom_dashboard_widget_postie", $widget_title, array($this, 'display_widget')); //, '', $post_id
+		}
+	}
+
+	function display_widget($post) //, $args
+	{
+		$postie_settings = get_option('postie-settings');
+		$postie_address = (is_array($postie_settings) && isset($postie_settings['mail_userid']) ? $postie_settings['mail_userid'] : "");
+
+		//echo var_export($postie_settings, true);
+
+		if($postie_address != '')
+		{
+			$user_data = get_userdata(get_current_user_id());
+			$profile_email = $user_data->user_email;
+
+			echo "<p>".sprintf(__("If you want to create a post directly from your e-mail client you can send a message to %s and it will be created as soon as the server has received the message. It is important that you send the message from your address %s and the message should only contain exactly the text that you want the post to contain. If you attach an image it will also be published with the post.", $this->lang_key), "<a href='mailto:".$postie_address."'>".$postie_address."</a>", "<a href='mailto:".$profile_email."'>".$profile_email."</a>")."</p>";
+		}
+
+		else if(IS_ADMIN)
+		{
+			echo "<p>".sprintf(__("There is no e-mail address set. Please go to the settings and enter the correct e-mail address and corresponding server settings.", $this->lang_key), "<a href='".admin_url("admin.php?page=postie-settings")."'>", "</a>")."</p>";
 		}
 	}
 
